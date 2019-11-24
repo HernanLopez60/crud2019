f916c1bff451c6108219d67bd2b936dd93e365ca
HernanLopez60 <hl3918027@gmail.com>
2019-11-24 16:12:46 +0100

    8commit

--- app/src/main/java/com/example/crudmysqlandroid/Dto.java
+++ app/src/main/java/com/example/crudmysqlandroid/Dto.java
@@ -5,9 +5,7 @@ import java.io.Serializable;
 
 //Para enviar objetos entre actividades (Como parámetro) se coloca el "implements Serializable"
 public class Dto implements Serializable {
-    int codigo;
-    String descripcion;
-    double precio;
+
 
 
     public Dto() {
